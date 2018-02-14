2cc0411423b13e5e5f554f4dfcc1c37df981bfab
Andrew Farrell <ahfarrell@sparkl.com>
2018-02-14 22:35:27 +1300

    intro of scenario testing

--- bluesky/test/scenario/test_synsimple.py
+++ bluesky/test/scenario/test_synsimple.py
@@ -12,20 +12,17 @@ from . import init_snaplog, get_snaplog_contents
 from .. import assert_fl
 
 
-def synsimple_validate(snapshot_):
-    # The new values for lat ([3]) or lon ([4]) are
-    # in fact deterministic based on the simulation being
-    # for roughly 60 seconds, where simdt is the same.
+def synsimple_validate(snapshot_, thresh=.025):
     # Expressing a tight range is sufficient to show the
     # direction of travel of the aircraft.
     #
     snapshot = snapshot_.split(',')
     if snapshot[1] == 'OWNSHIP':
-        assert_fl(snapshot[3], -.4, .025)
+        assert_fl(snapshot[3], -.4, thresh)
         assert float(snapshot[4]) == 0.0
     else:
         assert float(snapshot[3]) == 0.0
-        assert_fl(snapshot[4],  .4, .025)
+        assert_fl(snapshot[4],  .4, thresh)
 
 
 def test_synsimple(sock, snaplog):
@@ -40,5 +37,5 @@ def test_synsimpled(sock, snaplog):
     init_ = init_snaplog(sock, snaplog)
     sock_send(sock, "SYN SIMPLED")
     contents = get_snaplog_contents(sock, snaplog, init_)
-    synsimple_validate(contents[-2])
-    synsimple_validate(contents[-1])
+    synsimple_validate(contents[-2], .1)
+    synsimple_validate(contents[-1], .1)
